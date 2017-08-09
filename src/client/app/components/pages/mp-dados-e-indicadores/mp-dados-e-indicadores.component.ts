58eaeba56413fbe12cd5f0eb2065bc804a3859ee
Marcos <mag.junior@hotmail.com>
2017-08-09 12:20:56 -0300

    Commit intermediário

--- src/client/app/components/pages/mp-dados-e-indicadores/mp-dados-e-indicadores.component.ts
+++ src/client/app/components/pages/mp-dados-e-indicadores/mp-dados-e-indicadores.component.ts
@@ -6,6 +6,8 @@ import { Observable } from 'rxjs/Observable';
 import { Injector } from '@angular/core';
 import { Config, RouterExtensions } from '../../../modules/core/index';
 
+import { Chart } from 'chart.js';
+
 @Component({
   moduleId: module.id,
   selector: 'mp-dados-e-indicadores',
@@ -14,6 +16,26 @@ import { Config, RouterExtensions } from '../../../modules/core/index';
 })
 export class MPDadosEIndicadoresComponent {
 
-  constructor(private injector: Injector, public routerext: RouterExtensions) {}
+  data: any;
+
+  constructor(private injector: Injector, public routerext: RouterExtensions) {
+    this.data = {
+      labels: ['A','B','C'],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ]
+        }]
+    };
+  }
 
 }
