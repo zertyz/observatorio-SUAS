267051475ed8891340ed400e41932554199167d4
Breno <breno.esilva@outlook.comm>
2017-08-17 10:44:30 -0300

    criação do p-accordion e ajustes de tamanho e fonte na página ajuda.

--- src/client/app/components/pages/mp-ajuda/mp-ajuda.component.ts
+++ src/client/app/components/pages/mp-ajuda/mp-ajuda.component.ts
@@ -2,6 +2,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
+import {AccordionModule} from 'primeng/primeng';
 
 import { Injector } from '@angular/core';
 import { Config, RouterExtensions } from '../../../modules/core/index';
