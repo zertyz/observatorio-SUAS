43f1410be84f12be7062035dcb71b9bd9a8563aa
coppolaop <mag.junior@hotmail.com>
2017-08-21 14:21:10 -0300

    Inclusão do Multiselect na pagina de inspeções

--- src/client/app/components/pages/mp-inspecoes/mp-inspecoes.component.ts
+++ src/client/app/components/pages/mp-inspecoes/mp-inspecoes.component.ts
@@ -2,6 +2,9 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
+import {MultiSelectModule} from 'primeng/primeng';
+import {SelectItem} from 'primeng/primeng';
+
 
 import { Injector } from '@angular/core';
 import { Config, RouterExtensions } from '../../../modules/core/index';
@@ -14,6 +17,14 @@ import { Config, RouterExtensions } from '../../../modules/core/index';
 })
 export class MPInspecoesComponent {
 
-  constructor(private injector: Injector, public routerext: RouterExtensions) {}
+  eixos: SelectItem[];
+  selectedEixos: string[];
+
+  constructor(private injector: Injector, public routerext: RouterExtensions) {
+    this.eixos = [];
+    this.eixos.push({label:'EQUIPE TÉCNICA', value:'EQUIPE TÉCNICA'});
+    this.eixos.push({label:'INFRAESTRUTURA', value:'INFRAESTRUTURA'});
+    this.eixos.push({label:'PROGRAMAS, PROJETOS, SERVIÇOS E BENEFÍCIOS', value:'PROGRAMAS, PROJETOS, SERVIÇOS E BENEFÍCIOS'});
+  }
 
 }
