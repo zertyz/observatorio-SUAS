c6a56f202748b6cb711c6ffdeb8c6ae485af9f9a
brenoES <breno.esilva@outlook.com>
2017-09-05 11:26:30 -0300

    equipamentos de dados e indicadores agora estão sendo apresentadado como paginação.(faltam alguns ajustes).

--- src/client/app/components/pages/mp-dados-e-indicadores/mp-dados-e-indicadores.component.ts
+++ src/client/app/components/pages/mp-dados-e-indicadores/mp-dados-e-indicadores.component.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
+import {DataGridModule} from 'primeng/primeng';
 
 import { ActivatedRoute } from '@angular/router';
 
