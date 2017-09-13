6d143e73255648db972d7fcd8067b5316ce3af2b
Marcos <mag.junior@hotmail.com>
2017-09-13 05:25:14 -0300

    Inicio da programação do email da tela de ajuda (ainda incompleto)

--- src/client/app/components/pages/mp-ajuda/mp-ajuda.component.ts
+++ src/client/app/components/pages/mp-ajuda/mp-ajuda.component.ts
@@ -1,3 +1,5 @@
+//Criado com base no componente mp-subscribe
+
 // libs
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
@@ -7,6 +9,12 @@ import {AccordionModule} from 'primeng/primeng';
 import { Injector } from '@angular/core';
 import { Config, RouterExtensions } from '../../../modules/core/index';
 
+interface SubscriptionHttpPostObject {
+  nome:     string;
+  email:    string;
+  mensagem: string;
+}
+
 @Component({
   moduleId: module.id,
   selector: 'mp-ajuda',
@@ -15,6 +23,48 @@ import { Config, RouterExtensions } from '../../../modules/core/index';
 })
 export class MPAjudaComponent {
 
+  /* http */
+  private httpPostOptions: any             = {headers: {'Content-Type': 'application/json'}};
+  //private httpPostUrl: string              = 'http://apps.mprj.mp.br/eci/api/Aux/mailHLP';
+  private httpPostUrl: string              = '/eci/api/Aux/mailHLP';  // depende do proxy configurado em 'project.config.ts'
+  private subscriptionErrorMessage: string = null;
+
+  public isPosting:  boolean = false;
+  public subscribed: boolean = false;
+  public nome:      string  = "";
+  public email:      string  = "";
+  public mensagem:      string  = "";
+
   constructor(private injector: Injector, public routerext: RouterExtensions) {}
+
+  subscribe() {
+    this.isPosting  = true;
+    let postData: SubscriptionHttpPostObject = {
+      nome:     this.nome,
+      email:    this.email,
+      mensagem: this.mensagem,
+    }
+    let p: Observable<SubscriptionHttpPostObject> = this.http.post(this.httpPostUrl, postData, this.httpPostOptions)
+      .map(this.handleHttpPost)
+      .catch(this.handleHttpError);
+    p.subscribe(
+      postResult => {                                                         this.isPosting = false; this.subscribed = true;},
+      error      => {this.subscriptionErrorMessage = <any>error;              this.isPosting = false; this.subscribed = false;}
+    );
+  }
+
+  public handleHttpPost(res: Response) {
+    this.isPosting  = false;
+    this.subscribed = true;
+    let body = res.json();
+    return body.data || {};
+  }
+
+  public handleHttpError(error: Response | any) {
+    this.isPosting                = false;
+    this.subscribed               = false;
+    this.subscriptionErrorMessage = (error.message || error);
+    return Observable.throw(error.message || error);
+  }
 
 }
