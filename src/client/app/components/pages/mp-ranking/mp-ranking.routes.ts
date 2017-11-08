71f60ff921112c87dd877fc98af077e678ab9205
Marcos <mag.junior@hotmail.com>
2017-11-08 07:30:47 -0200

    avanço na utilização dos componentes do ECI

--- src/client/app/components/pages/mp-ranking/mp-ranking.routes.ts
+++ src/client/app/components/pages/mp-ranking/mp-ranking.routes.ts
@@ -1,8 +1,6 @@
 import { MPRankingComponent } from './mp-ranking.component';
 
 export const MPRankingRoutes: Array<any> = [
-  {
-    path: 'ranking',
-    component: MPRankingComponent
-  }
+  {component:  MPRankingComponent,          path: 'ranking/:municipio/:dimensao'},
+  {redirectTo: '/ranking/0/geral', path: 'ranking'},
 ];
