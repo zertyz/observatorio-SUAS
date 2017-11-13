09bbbc3f977f93289840fdc69c493fe3e8ca0283
Marcos <mag.junior@hotmail.com>
2017-11-13 13:15:47 -0200

    Implementação do TOP10 e Alterações solicitadas na página Home

--- src/client/app/components/pages/mp-home/mp-home.component.ts
+++ src/client/app/components/pages/mp-home/mp-home.component.ts
@@ -6,6 +6,13 @@ import { Observable } from 'rxjs/Observable';
 import { Injector } from '@angular/core';
 import { Config, RouterExtensions } from '../../../modules/core/index';
 
+// services
+import { RankingsService } from '../../../shared/modules/m-edificando-o-controle-interno/services/rankings.service';
+import { IRankings }       from '../../../shared/modules/m-edificando-o-controle-interno/services/IRankings';
+
+// module libs
+import { GradacoesDeCores } from '../../../shared/modules/m-edificando-o-controle-interno/GradacoesDeCores';
+
 @Component({
   moduleId: module.id,
   selector: 'mp-home',
@@ -14,6 +21,21 @@ import { Config, RouterExtensions } from '../../../modules/core/index';
 })
 export class MPHomeComponent {
 
-  constructor(private injector: Injector, public routerext: RouterExtensions) {}
+  public top10Cidades: string[] = ['§vazio§', '§vazio§', '§vazio§', '§vazio§', '§vazio§', '§vazio§', '§vazio§', '§vazio§', '§vazio§', '§vazio§'];
+  public top10Notas:   number[] = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
+
+  public errorMessage: string = null;
+
+  // constroi a estrutura 'top10Cidades'
+  constructor(private rankingsService: RankingsService,
+              private gradacoes: GradacoesDeCores) {
+    rankingsService.fetchRankings().subscribe(response => {
+      let rankings: IRankings[] = response.sort( (e1, e2) => e2.geral - e1.geral);
+      for (let i = 0; i < this.top10Cidades.length; i++) {
+        this.top10Cidades[i] = rankings[i].municipio;
+        this.top10Notas[i]   = rankings[i].geral;
+      }
+    }, error => this.errorMessage = < any > error);
+  };
 
 }
